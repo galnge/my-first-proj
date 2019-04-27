436d738c13ad4264fdaaa7fe910466d52e428fb9
Fera <feraziza@gmail.com>
2019-04-27 15:52:03 +0300

    deleted ng

--- ng/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NgConfig(AppConfig):
-    name = 'ng'
--- ng/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-import datetime
-
-def current_datetime(request,name):
-    html = f"<html><body>{name} ПИДОР </body></html>"
-    return HttpResponse(html)
