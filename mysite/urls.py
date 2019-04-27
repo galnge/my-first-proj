0b2900f3ae62ede419ded9197938dcff5bdc403b
Fera <feraziza@gmail.com>
2019-04-27 16:10:09 +0300

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ng.views import current_datetime
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
